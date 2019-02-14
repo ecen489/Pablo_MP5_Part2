55257f3447178868e9645c2aed189869b3be1f13
Pablo Romo <pabromo@tamu.edu>
2019-02-14 16:46:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pablo_mp5_part2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pablo_mp5_part2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // THIS IS THE QUIZ APP
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
